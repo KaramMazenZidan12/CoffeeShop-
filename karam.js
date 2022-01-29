f339aca3a6c0dea66151bfac4c98eda48be6b270
KaramMazenZidan12 <98523370+KaramMazenZidan12@users.noreply.github.com>
2022-01-29 15:56:03 +0200

    Add files via upload

---
+++ karam.js
@@ -0,0 +1,64 @@
+function k (){
+
+window.location.replace("products.html#d");
+
+
+}
+function k1 (){
+
+    window.location.replace("products.html#f");
+    
+    
+    }
+    function k2 (){
+
+        window.location.replace("products.html#g");
+        
+        
+        }
+        function k3 (){
+
+            window.location.replace("products.html#h");
+            
+            
+            }
+            function k4 (){
+
+                window.location.replace("products.html#j");
+                
+                
+                }
+                function k5 (){
+
+                    window.location.replace("products.html#k");
+                    
+                    
+                    }
+                    function k6 (){
+
+                        window.location.replace("index.html#tt");
+                        
+                        
+                        }
+                        function k7 (){
+
+                           var h=document.getElementsByTagName("a");
+                           for(let i=0;i<h.length;i++){
+                            h[i].style.fontSize="40px";
+                           }
+                           
+                            
+                            
+                            }
+                        
+                            function k8 (){
+
+                                var h=document.getElementsByTagName("a");
+                               
+                                for(let i=0;i<h.length;i++){
+                                    h[i].style.fontSize="25px";
+                                   }
+                               
+                                 
+                                 }
+                             
